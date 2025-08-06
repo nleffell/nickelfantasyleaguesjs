f32be3a142711b9213205fb9db20b5e1e6c36b64
nleffell <85805123+nleffell@users.noreply.github.com>
2025-08-05 22:42:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ async function createCurrentDraftPickOrderTable() {
   const currentDraftPickOrderRes = await fetch("https://scripts.nickelfantasyleagues.com/wbdw_jsons/website_jsons/current_draft_pick_order.json");
   const json = await currentDraftPickOrderRes.json();
      
-  let tableContainer = document.querySelector('div.test')
+  let tableContainer = document.querySelector('div.div-wbdw-home-draft-pick-order') || document.querySelector('div.div-wbdw-home-draft-pick-order-post-season')
 
   const table = document.createElement('table');
   table.classList.add('table-wbdw-draft-pick-order');
