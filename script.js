5d3a458a9519e32bd06870cebafe7d4d976fce00
nleffell <85805123+nleffell@users.noreply.github.com>
2025-08-30 21:29:13 -0400

    Update script.js
    changed function name

--- script.js
+++ script.js
@@ -752,7 +752,7 @@ async function createPreseasonChampionshipOdds() {
 
 
 //#######Owner Page Functions#######
-async function createOwnerRecords() {
+async function createOwnerStats() {
   const statsRes = await fetch("https://scripts.nickelfantasyleagues.com/wbdw_jsons/website_jsons/owner_aggregate_records.json");
   const json = await statsRes.json();
 
